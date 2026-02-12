2e224deba4cc20fb7199473d77ed3ee43038641f
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-02-12 15:16:01 -0300

    feat: atualizar estrutura do componente App e adicionar novos componentes de Link e SocialLinks

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
+import Link from './components/Link/Link'
 import Perfil from './components/Perfil/Perfil'
+import SocialLinks from './components/SocialLinks/SocialLinks'
 
 function App() {
   return (
-    <div id='App'>
-      <Perfil />
-    
-      <div className='switch'>botão switch</div>
-      <div className='Links'></div>
-      <div className='socialLinks'></div>
-      <div className='rodape'></div>
+    <div id="App">
+      <Perfil fotoPerfil={"https://placehold.co/100"}>Ray</Perfil>
+      <div className="switch">botão switch</div>
+      <ul>
+            <Link url={""}>Instagram</Link>
+            <Link url={""}>Minha Playlist</Link>
+            <Link url={""}>Me pague um café</Link>
+           <Link url={""}>Conheça o curso do DEV</Link>
+      </ul>
+      <div className="Links"></div>
+      <div className="SocialLinks"></div>
+      <div className="rodape"></div>
     </div>
   )
 }
--- 03-devlinks/src/components/Perfil/Perfil.jsx
+++ 03-devlinks/src/components/Perfil/Perfil.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Perfil = () => {
+const Perfil = ({children, fotoPerfil}) => {
   return (
       <div className='Perfil'>
-        <img src="https://placehold.co/200x200" alt="" />
-        <p>@Seu Nome</p>
+        <img src={fotoPerfil} alt="" />
+        <p>@{children}</p>
       </div>
   )
 }
