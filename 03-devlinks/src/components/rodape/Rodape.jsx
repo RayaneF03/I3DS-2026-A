c15a8071e443892992aed13e3bf68b1b2c3562ef
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-02-12 16:52:51 -0300

    feat: adicionar componente Rodape com mensagem personalizada

---
+++ 03-devlinks/src/components/rodape/Rodape.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from 'react'
+
+const Rodape = () => {
+  return (
+   <footer>
+    <P>Feito com 🩵 por <a href="https://github.com">{Children}</a></P>
+   </footer>
+  )
+}
+
+export default Rodape
