10deb6732959a99d09858e670e74277d286dc268
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-02-19 17:03:34 -0300

    feat: implementar busca de filmes na API OMDB e adicionar arquivo de exemplo .env

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import Rodape from "./assets/components/Rodape/Rodape";
@@ -5,6 +6,25 @@ import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+  //Criando a conexão com a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json;
+
+    //Alimentando a variavel movies
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+
+
   return (
     <div id="App">
       <img
@@ -16,6 +36,10 @@ const App = () => {
       <div className="search">
         <input type="text" placeholder="Pesquise por filmes e séries" />
         <img src={lupa} alt="Botão de ação de pesquisa" />
+      </div>
+
+      <div className="container">
+        {movies.map}
       </div>
 
       <Rodape link={"https://github.com/RayaneF03"}>Rayane</Rodape>
