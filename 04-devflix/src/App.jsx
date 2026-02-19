8ba17811dd0e2be5f91c50ff83320936ae207960
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-02-19 11:20:51 -0300

    feat: adicionar componente Rodape com estilos e estrutura

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,22 +1,23 @@
 import "./App.css";
-
-import logo from "./assets/devflix.png"
-import lupa from "./assets/search.svg"
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
 
 const App = () => {
   return (
     <div id="App">
-      <img className="logo" src={logo} alt="Imagem do logo da Netflix com fundo preto e letras vermelhas, promovendo a plataforma de streaming de filmes e séries." />
-      
+      <img
+        className="logo"
+        src={logo}
+        alt="Imagem do logo da Netflix com fundo preto e letras vermelhas, promovendo a plataforma de streaming de filmes e séries."
+      />
+
       <div className="search">
-        <input
-          type="text"
-          placeholder="Pesquise por filmes e séries"
-        />
+        <input type="text" placeholder="Pesquise por filmes e séries" />
         <img src={lupa} alt="Botão de ação de pesquisa" />
       </div>
+      <rodape>Ray</rodape>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
