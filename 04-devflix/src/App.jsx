3d84f3e9ee5bbe006937abdacb0e8ab839803b1e
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-02-19 11:04:41 -0300

    feat: adicionar estrutura básica do componente App com logo e campo de pesquisa

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
 
+import logo from "./assets/devflix.png"
+import lupa from "./assets/search.svg"
+
 const App = () => {
   return (
-    <div>
+    <div id="App">
+      <img className="logo" src={logo} alt="Imagem do logo da Netflix com fundo preto e letras vermelhas, promovendo a plataforma de streaming de filmes e séries." />
       
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Pesquise por filmes e séries"
+        />
+        <img src={lupa} alt="Botão de ação de pesquisa" />
+      </div>
     </div>
   )
 }
