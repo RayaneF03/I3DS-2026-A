22c42c8de41b57454d723adf69b8bde7fd0b765a
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-02-19 16:19:24 -0300

    feat: adicionar estrutura do componente App e implementar o componente MovieCard

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import Rodape from "./assets/components/Rodape/Rodape";
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
@@ -7,7 +8,7 @@ const App = () => {
   return (
     <div id="App">
       <img
-        className="logo"
+        id="Logo"
         src={logo}
         alt="Imagem do logo da Netflix com fundo preto e letras vermelhas, promovendo a plataforma de streaming de filmes e séries."
       />
@@ -16,9 +17,10 @@ const App = () => {
         <input type="text" placeholder="Pesquise por filmes e séries" />
         <img src={lupa} alt="Botão de ação de pesquisa" />
       </div>
+
       <Rodape link={"https://github.com/RayaneF03"}>Rayane</Rodape>
     </div>
   );
 };
 
-export default Rodape;
+export default App;
