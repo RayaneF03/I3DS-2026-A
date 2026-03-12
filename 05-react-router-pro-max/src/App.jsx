9d3e2ff27c21fd164c31d9383559f4d331e19b5d
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-03-12 16:53:13 -0300

    feat: initialize React Router project with basic structure and components
    - Added package.json for project dependencies and scripts
    - Created main application structure with App component
    - Implemented routing with Home, Sobre, Contato, and NaoEncontrado pages
    - Developed Header and Rodape components for navigation and footer
    - Styled components with CSS and Bootstrap
    - Configured Vite for development and build processes

---
+++ 05-react-router-pro-max/src/App.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
+import NaoEncontrado from "./pages/NaoEncontrado";
+import Header from "./components/header/Header";
+import Rodape from "./components/rodape/Rodape";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+      <Routes>
+        {/* Identifica todas as rotas do sistema*/}
+        <Route path="/Contato" element={<Contato />} />
+        <Route path="*" element={<NaoEncontrado />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Sobre" element={<Sobre />} /> {/* uma rota do sistema*/}
+
+      </Routes>
+</div>
+
+      <Rodape link={"https://github.com/RayaneF03"}>RayaneF03</Rodape>
+      
+    </>
+  );
+};
+
+export default App;
